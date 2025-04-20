f07722e7cc697db8fd45082fd6cb2001f46a0369
Saahi Venkat <88107847+s444hi@users.noreply.github.com>
2025-04-19 20:04:37 -0700

    Add files via upload

---
+++ removeElement.java
@@ -0,0 +1,15 @@
+/**
+ * @author Saahi Venkatraghavan
+ */
+public class removeElement {
+    public int removeDuplicates(int[] nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
